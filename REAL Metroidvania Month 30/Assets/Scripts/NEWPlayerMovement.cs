45788890c2dc8f68c6f31d8b38aeb8f530d533b5
Quinn McDonnell <quinnmcd2003@gmail.com>
2025-12-05 16:14:36 -0500

    Better fall and movement, next to add coyote time

--- REAL Metroidvania Month 30/Assets/Scripts/NEWPlayerMovement.cs
+++ REAL Metroidvania Month 30/Assets/Scripts/NEWPlayerMovement.cs
@@ -10,9 +10,23 @@ public class NEWPlayerMovement : MonoBehaviour
     public LayerMask ground;
 
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
+    [SerializeField] public float speed = 8f;
+    [SerializeField] public float jumpingPower = 16f;
+    
+    //Friction
+    [SerializeField] public float airFriction = 0.75f;
+    [SerializeField] public float groundFriction = 20f;
+    
+    //Gravity Modifiers
+    [SerializeField] public float fallGravMult = 3f;
+    [SerializeField] public float LowJumpGravMult = 2f;
+
+    //Jump
+    [SerializeField] private float jumpBufferTime = 0.15f;
+    private float jumpBufferCounter;
+
     private bool isFacingRight = true;
+    private bool isJumpHeld;
 
     private void Awake()
     {
@@ -23,7 +37,23 @@ public class NEWPlayerMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+        if(Mathf.Abs(horizontal) > 0.01f)
+        {
+            if (IsGrounded())
+            {
+                rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+            }
+            else
+            {
+                rb.linearVelocity = new Vector2(horizontal * speed * airFriction, rb.linearVelocity.y);
+            }
+        }
+        
+        if(IsGrounded() && Mathf.Abs(horizontal) < 0.01f)
+        {
+            float newX = Mathf.MoveTowards(rb.linearVelocity.x, 0, groundFriction * Time.fixedDeltaTime);
+            rb.linearVelocity = new Vector2(newX, rb.linearVelocity.y);
+        }
 
         if(!isFacingRight && horizontal > 0f)
         {
@@ -33,6 +63,19 @@ public class NEWPlayerMovement : MonoBehaviour
         else if (isFacingRight && horizontal < 0f)
         {
             Flip();
+        }
+
+        if(rb.linearVelocity.y < 0f)
+        {
+            rb.gravityScale = fallGravMult;
+        } 
+        else if(rb.linearVelocity.y > 0f && !isJumpHeld)
+        {
+            rb.gravityScale = LowJumpGravMult;
+        }
+        else
+        {
+            rb.gravityScale = 1.0f;
         }
     }
 
@@ -51,6 +94,8 @@ public class NEWPlayerMovement : MonoBehaviour
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
         }
+
+        isJumpHeld = !context.canceled;
     }
 
     private bool IsGrounded()
